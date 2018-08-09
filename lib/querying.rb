a6234ab01c96c58b5bc8320fa6408a6b1a4ab6b3
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-08-09 00:47:13 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,37 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "select series.title, series.year where series.series_id = '1' order by series.year; "
+  "select title, year from books where series_id = '1' order by year; "
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "select name, motto from characters order by length(motto) desc limit 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "select species, count(*) from characters group by species order by count(species) desc limit 1"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "select authors.name, subgenres.name from authors join series on series.author_id = authors.id join subgenres on series.subgenre_id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  <<-SQL
+      SELECT series.title
+      FROM series
+      JOIN books
+      ON books.series_id = series.id
+      JOIN character_books
+      ON character_books.book_id = books.id
+      JOIN characters
+      ON character_books.character_id = characters.id
+      WHERE characters.species = 'human'
+      GROUP BY series.title
+      ORDER BY COUNT(*) DESC
+      LIMIT 1
+    SQL
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "select characters.name, count(character_books.book_id) from characters join character_books on character_books.character_id = characters.id group by characters.name order by count(character_books.book_id) desc"
 end
